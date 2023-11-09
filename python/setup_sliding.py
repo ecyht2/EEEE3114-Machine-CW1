acb8fd298c623791f42710d753be66e64c7f0240
ecyht2 <ecyht2@nottingham.edu.my>
2023-11-09 17:14:55 +0800

    feat: Added sliding band simulation

---
+++ python/setup_sliding.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+"""Document creation and setup"""
+import math
+import os
+
+import femm  # type: ignore
+
+
+def add_label(
+    x: float,
+    y: float,
+    mat: str,
+    circ: str = "",
+    magdir: float = 0,
+    group: int = 0,
+    turns: int = 0,
+):
+    """Adds a new label to the problem."""
+    # pylint: disable=too-many-arguments
+    femm.mi_addblocklabel(x, y)
+    femm.mi_selectlabel(x, y)
+    femm.mi_setblockprop(mat, 1, 0, circ, magdir, group, turns)
+    femm.mi_clearselected()
+
+
+def add_boundary(coords: list[float], name: str):
+    """Adds new boundaries."""
+    for index, coord in enumerate(coords):
+        boundary_name = name.format(index)
+        femm.mi_addboundprop(boundary_name, *[None for _ in range(8)], 5)
+        femm.mi_selectsegment(coord, 0)
+        femm.mi_selectsegment(0, coord)
+        femm.mi_setsegmentprop(boundary_name, None, None, None, None)
+        femm.mi_clearselected()
+
+
+def cosd(value: float) -> float:
+    """Calculate the cosine using degrees."""
+    return math.cos(math.radians(value))
+
+
+def sind(value: float) -> float:
+    """Calculate the sine using degrees."""
+    return math.sin(math.radians(value))
+
+
+if __name__ == "__main__":
+    femm.openfemm(
+        winepath="/usr/bin/wine",
+        femmpath="/home/user/.local/share/wineprefixes/default/drive_c/femm42/bin/",
+    )
+    femm.newdocument(0)
+    femm.mi_probdef(None, "millimeters", "planar", None, 100)
+    femm.smartmesh(0)
+
+    # Importing
+    femm.mi_readdxf("../cw_sliding.dxf")
+    femm.mi_zoomnatural()
+
+    # Importing Materials
+    AIR = "Air"
+    IRON = "Pure Iron"
+    N42 = "N42"
+    STEEL_1018 = "1018 Steel"
+    STEEL_M19 = "M-19 Steel"
+    COPPER = "10 AWG"
+    femm.mi_getmaterial(AIR)
+    femm.mi_getmaterial(IRON)
+    femm.mi_getmaterial(N42)
+    femm.mi_getmaterial(STEEL_1018)
+    femm.mi_getmaterial(STEEL_M19)
+    femm.mi_getmaterial(COPPER)
+
+    # Adding Circuits
+    femm.mi_addcircprop("A", 0, 1)
+    femm.mi_addcircprop("B", 0, 1)
+    femm.mi_addcircprop("C", 0, 1)
+
+    # Adding Static Boundary
+    femm.mi_addboundprop("Boundary")
+    femm.mi_selectarcsegment(62.5, 0)
+    femm.mi_setarcsegmentprop(5, "Boundary", None, None)
+    femm.mi_clearselected()
+    # Sliding Boundary
+    femm.mi_addboundprop("Sliding Boundary", *[None for _ in range(8)], 7)
+    femm.mi_selectarcsegment(24.7, 0)
+    femm.mi_selectarcsegment(24.3, 0)
+    femm.mi_setarcsegmentprop(5, "Sliding Boundary", None, None)
+    femm.mi_clearselected()
+    # Moving Boundary
+    # Rotor
+    add_boundary([6.25, 16.25], "Rotor Boundary {}")
+    # Air Gap 1
+    add_boundary([22, 25], "Air Gap {}")
+    # Air Gap 2
+    # Stator Boundary
+    add_boundary([46.5, 56.25], "Stator Boundary {}")
+    # Setting Rotor Group
+    femm.mi_selectcircle(0, 0, 24.5, 4)
+    femm.mi_setgroup(1)
+
+    # Adding Materials
+    # Rotor Inner
+    add_label(6.25 * cosd(45), 6.25 * sind(45), STEEL_1018, group=1)
+    # Rotor Outer
+    add_label(16.25 * cosd(45), 16.25 * sind(45), STEEL_M19, group=1)
+    # Air Gap
+    add_label(24.8 * cosd(45), 24.8 * sind(45), AIR)
+    add_label(22 * cosd(15), 22 * sind(15), AIR)
+    # Magnets
+    x_val = 22 * cosd(45)
+    y_val = 22 * sind(45)
+    add_label(x_val, y_val, N42, magdir=45, group=1)
+    # Stator Radius
+    add_label(46.5 * cosd(45), 46.5 * sind(45), STEEL_M19)
+    # Outer Cage Radius
+    add_label(56.25 * cosd(45), 56.25 * sind(45), IRON)
+    # Windings/Slots
+    # Middle Full
+    circuits = ["A", "A", "B", "B", "C", "C"]
+    DIFF = 360 / 24
+    for i, circuit in enumerate(circuits):
+        x_val = 34 * cosd(i * DIFF + DIFF / 2)
+        y_val = 34 * sind(i * DIFF + DIFF / 2)
+        add_label(x_val, y_val, COPPER, circ=circuit, turns=40)
+
+    # Document saving
+    os.makedirs("../dist", exist_ok=True)
+    femm.mi_saveas("../dist/cw1_sliding.fem")
+    femm.closefemm()
---
+++ python/task_1_2_sliding.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+"""Test script."""
+import csv
+import logging
+import os
+from dataclasses import dataclass
+from multiprocessing import Process, Queue
+
+import femm  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib import femm_handler
+
+
+@dataclass
+class TaskData:
+    """The output data format for the collected data."""
+    angle: float
+    coggingtorque: float
+    aflux: float
+    bflux: float
+    cflux: float
+
+
+@femm_handler("../dist/cw1_sliding.fem")
+def task_1_2(initial_angle: int, count: int, out: Queue):
+    """Function to get data for Task 1 and 2.
+
+    :param initial_angle: Initial angle of the measurement.
+    :param count: The amount of times to rotate the rotor.
+    :param out: The queue to output the collected data to.
+    """
+    femm.mi_modifycircprop("A", 1, 0)
+    femm.mi_modifycircprop("B", 1, 0)
+    femm.mi_modifycircprop("C", 1, 0)
+    femm.smartmesh(1)
+
+    for k in range(count):
+        current_angle = initial_angle + k
+        # Debug
+        logging.info("Angle: %s", current_angle)
+
+        femm.mi_modifyboundprop("Sliding Boundary", 10, current_angle)
+        # Anlyzing
+        femm.mi_analyze(1)
+        femm.mi_loadsolution()
+
+        # Gathering Data
+        tq = femm.mo_gapintegral("Sliding Boundary", 0)
+        circprops_a = femm.mo_getcircuitproperties("A")
+        circprops_b = femm.mo_getcircuitproperties("B")
+        circprops_c = femm.mo_getcircuitproperties("C")
+        out.put(
+            TaskData(
+                current_angle,
+                tq,
+                circprops_a[2],
+                circprops_b[2],
+                circprops_c[2],
+            )
+        )
+
+        # Setting up for next cycle
+        femm.mo_close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    queue: Queue = Queue()
+    processes: list[Process] = []
+    THREADS = 10
+    COUNT = round(360 / THREADS)
+
+    for i in range(THREADS):
+        p = Process(target=task_1_2, args=(i * COUNT, COUNT, queue))
+        p.start()
+        processes.append(p)
+
+    for process in processes:
+        process.join()
+
+    RPM = 1500
+    OMEGA = RPM * 360 / 60
+    DT = 1 / OMEGA
+
+    tt = np.zeros(360)
+    aflux = np.zeros(360)
+    bflux = np.zeros(360)
+    cflux = np.zeros(360)
+    coggingtorque = np.zeros(360)
+
+    while queue.qsize() > 0:
+        item: TaskData = queue.get()
+        angle = int(item.angle)
+        tt[angle] = angle
+        aflux[angle] = item.aflux
+        bflux[angle] = item.bflux
+        cflux[angle] = item.cflux
+        coggingtorque[angle] = item.coggingtorque
+
+    plt.figure(1)
+    plt.plot(tt, coggingtorque)
+    plt.xlabel("Angle, °")
+    plt.ylabel("Cogging Torque, N*m")
+    plt.title("Cogging Torque")
+    plt.xlim(0, 360)
+    plt.savefig("../dist/task_1.png")
+
+    plt.figure(2)
+    va = 4 * np.diff(aflux) / DT
+    vb = 4 * np.diff(bflux) / DT
+    vc = 4 * np.diff(cflux) / DT
+    td = tt[1:] / OMEGA - DT / 2
+    plt.plot(td, va, td, vb, td, vc)
+    plt.xlabel("Time, Seconds")
+    plt.ylabel("Phase-to-Neutral Voltage")
+    plt.title("Phase Voltage")
+    plt.xlim(min(td), max(td))
+    plt.savefig("../dist/task_2_1.png")
+
+    plt.figure(3)
+    vll = va - vc
+    plt.plot(td, vll)
+    plt.xlabel("Time, Seconds")
+    plt.ylabel("Line-to-Line Voltage")
+    plt.title("Line-to-Line Voltage")
+    plt.xlim(min(td), max(td))
+    plt.savefig("../dist/task_2_2.png")
+
+    # Logging
+    logging.info("Cogging Torque: %s", coggingtorque)
+    logging.info("Flux A: %s", aflux)
+    logging.info("Flux B: %s", bflux)
+    logging.info("Flux C: %s", cflux)
+    logging.info("Va: %s", va)
+    logging.info("Vb: %s", vb)
+    logging.info("Vc: %s", vc)
+    logging.info("Vll: %s", vll)
+
+    os.makedirs("../dist", exist_ok=True)
+    with open("../dist/task_1.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Angle", "Cogging Torque"])
+        csv_array = np.transpose(np.array([tt, coggingtorque]))
+        writer.writerows(csv_array)
+
+    with open("../dist/task_2.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Time", "Va", "Vb", "Vc", "Vll"])
+        csv_array = np.transpose(np.array([td, va, vb, vc, vll]))
+        writer.writerows(csv_array)
+
+    # Finding frequency
+    fft = np.array(abs(np.fft.fft(coggingtorque)))
+    freq = np.fft.helper.fftfreq(coggingtorque.size, DT)
+    index = np.argmax(fft)
+    f = freq[index]
+    angle_f = OMEGA * f**-1
+    with open("../dist/task_1.txt", "w", encoding="utf-8") as file:
+        output = f"Cogging Troque Frequency: {angle_f}"
+        file.write(output)
+        logging.info(output)
+
+    plt.show()
