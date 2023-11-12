f3874fbe3bb5284a7190cd88a34371e9ae84979d
ecyht2 <ecyht2@nottingham.edu.my>
2023-11-12 12:34:34 +0800

    feat: Added task 4 and 5

---
+++ python/task_4.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+"""Script for task 4."""
+import csv
+import dataclasses
+import logging
+import multiprocessing
+import os
+from dataclasses import dataclass
+from multiprocessing import Pool, Queue
+
+import femm  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib import femm_handler
+
+
+@dataclass
+class TaskData:
+    """The output data format for the collected data."""
+
+    current: float
+    yoke: float
+    teeth: float
+    magnet: float
+
+
+def mag(b_values: tuple[float, float]):
+    """Finds the magnitude of the B field."""
+    return np.sqrt(b_values[0] ** 2 + b_values[1] ** 2)
+
+
+@femm_handler("../dist/cw1_sliding.fem")
+def task_4(current: float) -> TaskData:
+    """Function to get data for Task 4.
+
+    :param current: The current to measure.
+    :returns: The flux at important points.
+    """
+    thread_logger = multiprocessing.get_logger()
+    femm.smartmesh(1)
+
+    a = current * np.sin(np.radians(90))
+    b = current * np.sin(np.radians(90 + 120))
+    c = current * np.sin(np.radians(90 - 120))
+
+    # Debug
+    thread_logger.info("Current %s A", current)
+    thread_logger.debug("Current A: %s A", a)
+    thread_logger.debug("Current B: %s A", b)
+    thread_logger.debug("Current C: %s A", c)
+
+    femm.mi_modifycircprop("A", 1, a)
+    femm.mi_modifycircprop("B", 1, b)
+    femm.mi_modifycircprop("C", 1, c)
+
+    # Anlyzing
+    femm.mi_analyze(1)
+    femm.mi_loadsolution()
+
+    stator = np.zeros((2, 7), float)
+    for i in range(7):
+        teeth_angle = np.radians(i * 15 + 0.9)
+        stator[0, i] = mag(
+            femm.mo_getb(45 * np.cos(teeth_angle), 45 * np.sin(teeth_angle))
+        )
+
+        stator[1, i] = mag(
+            femm.mo_getb(43 * np.cos(teeth_angle), 43 * np.sin(teeth_angle))
+        )
+
+    magnet = np.zeros(46)
+    for mag_angle in range(30, 76):
+        magnet_x = 22 * np.cos(mag_angle)
+        magnet_y = 22 * np.sin(mag_angle)
+        magnet[mag_angle - 30] = mag(femm.mo_getb(magnet_x, magnet_y))
+
+    thread_logger.debug("Yoke Fluxes: %s", stator[0])
+    thread_logger.debug("Teeth Fluxes: %s", stator[1])
+    thread_logger.debug("Magnet Fluxes: %s", magnet)
+
+    output = TaskData(current, max(stator[0]), max(stator[1]), max(magnet))
+
+    femm.mo_close()
+
+    return output
+
+
+if __name__ == "__main__":
+    logger = multiprocessing.log_to_stderr()
+    queue: Queue = Queue()
+
+    currents = np.arange(11) * 20
+    with Pool(11) as p:
+        flux: list[TaskData] = p.map(task_4, currents)
+
+    flux.sort(key=lambda v: v.current)
+
+    # Debug
+    logger.debug("Fluxes: %s", flux)
+
+    os.makedirs("../dist", exist_ok=True)
+    with open("../dist/task_4.csv", "w", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(
+            csv_file, fieldnames=["current", "yoke", "teeth", "magnet"]
+        )
+        writer.writeheader()
+        writer.writerows(map(dataclasses.asdict, flux))
+
+    # Yoke
+    plt.plot(currents, list(map(lambda v: v.yoke, flux)), label="Yoke", marker="o")
+
+    # Teeth
+    plt.plot(currents, list(map(lambda v: v.teeth, flux)), label="Teeth", marker="o")
+
+    # Magnet
+    plt.plot(currents, list(map(lambda v: v.magnet, flux)), label="Magnet", marker="o")
+
+    # Labels
+    plt.xlabel("Current, A")
+    plt.ylabel("Flux, T")
+    plt.title("Current vs Flux")
+    plt.xlim(min(currents), max(currents))
+    plt.xticks(currents)
+    plt.legend()
+    plt.savefig("../dist/task_4.png")
+
+    plt.show()
---
+++ python/task_5.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""Script for task 5."""
+import csv
+import dataclasses
+import logging
+import multiprocessing
+import os
+from dataclasses import dataclass
+from multiprocessing import Pool, Queue
+
+import femm  # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib import femm_handler
+
+
+@dataclass
+class TaskData:
+    """The output data format for the collected data."""
+    current: float
+    torque: float
+
+
+@femm_handler("../dist/cw1_sliding.fem")
+def task_5(current: float) -> TaskData:
+    """Function to get data for Task 5.
+
+    :param current: The current to measure.
+    :returns: The mean torque developed.
+    """
+    thread_logger = multiprocessing.get_logger()
+    femm.smartmesh(1)
+
+    femm.mi_modifycircprop("A", 1, current * np.sin(np.radians(90)))
+    femm.mi_modifycircprop("B", 1, current * np.sin(np.radians(90 + 120)))
+    femm.mi_modifycircprop("C", 1, current * np.sin(np.radians(90 - 120)))
+
+    # Debug
+    thread_logger.info("Current: %s A", current)
+    thread_logger.debug("A: %s A", current * np.sin(np.radians(90)))
+    thread_logger.debug("B: %s A", current * np.sin(np.radians(90 + 120)))
+    thread_logger.debug("C: %s A", current * np.sin(np.radians(90 - 120)))
+
+    dev_torque = np.zeros(360)
+    for deg in range(360):
+        femm.mi_modifyboundprop("Sliding Boundary", 10, deg)
+        thread_logger.info("%s A, %s°", current, deg)
+        # Anlyzing
+        femm.mi_analyze(1)
+        femm.mi_loadsolution()
+        dev_torque[deg] = femm.mo_gapintegral("Sliding Boundary", 0)
+
+    thread_logger.debug("Torque: %s", dev_torque)
+    output = TaskData(current, np.mean(dev_torque, 0))
+
+    femm.mo_close()
+
+    return output
+
+
+if __name__ == "__main__":
+    logger = multiprocessing.log_to_stderr()
+    queue: Queue = Queue()
+
+    currents = np.arange(11) * 20
+    with Pool(11) as p:
+        torque: list[TaskData] = p.map(task_5, currents)
+
+    torque.sort(key=lambda v: v.current)
+    logger.debug("Data: %s", torque)
+
+    os.makedirs("../dist", exist_ok=True)
+    with open("../dist/task_5.csv", "w", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(
+            csv_file, fieldnames=["current", "torque"]
+        )
+        writer.writeheader()
+        writer.writerows(map(dataclasses.asdict, torque))
+
+    # Torque
+    plt.plot(currents, list(map(lambda v: v.torque, torque)), marker="o")
+
+    # Labels
+    plt.xlabel("Current, A")
+    plt.ylabel("Mean Torque, N*m")
+    plt.title("Current vs Mean Torque")
+    plt.xlim(min(currents), max(currents))
+    plt.xticks(currents)
+    plt.savefig("../dist/task_5.png")
+
+    plt.show()
