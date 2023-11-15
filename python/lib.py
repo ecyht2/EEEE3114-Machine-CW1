b3b3cb190c9d6454613c512b4a91a68984f4cecf
ecyht2 <ecyht2@nottingham.edu.my>
2023-11-15 11:46:38 +0800

    feat: Added new constants

--- python/lib.py
+++ python/lib.py
@@ -17,9 +17,13 @@ WINE_DIR = "/usr/bin/wine"
 RPM = 1500                  # Mechanical RPM
 OMEGA = RPM * 360 / 60      # Mechanical °/sec
 DT = 1 / OMEGA              # Mechanical sec/°
-F_E = 12 * RPM / 60         # Electrical Frequency
+F_E = 2 * RPM / 60          # Electrical Frequency
 OMEGA_E = 360 * F_E         # Electrical °/sec
 EDT = 1 / OMEGA_E           # Electrical sec/°
+MIDDLE = 52.5               # Angle of the middle of the magnet
+SLOT_ANGLE = 15             # Angle between each slot
+SLOT = 11.4                 # The total angle of a slot
+TEETH = 1.8                 # The total angle of a teeth
 
 
 def setup_femm() -> str:
