e90671dc2107e6446bcbde6b2ae6f65c9d9ed0dc
ecyht2 <ecyht2@nottingham.edu.my>
2023-11-15 23:23:56 +0800

    feat: Added helper function for test

--- python/test.py
+++ python/test.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 """Test script of coursework"""
+import csv
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_data(file_path: str) -> np.ndarray:
+    """Gets the data from a CSV file.
+
+    :param file_path: The file path of the CSV file.
+    """
+    with open(file_path, encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        output = np.fromiter(reader, dict)
+
+    for item in output:
+        for key in item.keys():
+            item[key] = float(item.get(key))
+
+    return output
+
+
+def plot_graph(x: list, y: list):
+    """Gets the data from a CSV file.
+
+    :param x: The items on the x-axis.
+    :param y: The items on the y-axis.
+    """
+    plt.plot(x, y)
+    plt.show()
+
+
 if __name__ == "__main__":
     pass
