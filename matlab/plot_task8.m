ad8a27e1daad0716045f13e0ce186424dbec9926
ecyht2 <ecyht2@nottingham.edu.my>
2023-12-23 16:57:35 +0800

    fix: plot_task8.m now saves in dist dir

--- matlab/plot_task8.m
+++ matlab/plot_task8.m
@@ -13,9 +13,11 @@ Xs = 6.029;
 Pd2 = 3 * (Eb * sqrt(20^2 * Xs^2 - Eb^2)) / Xs * sind(angle);
 Td2 = Pd2 / omega;
 
+mkdir("../dist/")
+
 plot(angle, Td);
 xlim([min(angle) max(angle)]);
 xlabel("Load Angle, °");
 ylabel("Torque Developed, Nm");
 title("Torque Developed at Different Load Angle");
-saveas(gcf(), "task_8_circuit.png");
+saveas(gcf(), "../dist/task_8_circuit.png");
